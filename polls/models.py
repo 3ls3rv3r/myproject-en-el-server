bfa02170e2434be199d59369ec2a6e24719334c9
Mi nombre <coach.umkt@gmail.com>
2010-10-20 23:02:35 -0500

    identificadores para los candidatos, corregi test votos para que buesque con ateg/opt

--- polls/models.py
+++ polls/models.py
@@ -3,122 +3,168 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from datetime import datetime
 
+#S: mejoras a models.Model
+class ModelGZM(models.Model):
+	def save(self,**kwargs):
+		# igual, pero devolvemos el objeto para permitir x= MiModelo(initval).save()
+		models.Model.save(self,**kwargs)
+		return self
+
 # Create your models here.
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __unicode__(self):
-        return self.question
-    def was_published_today(self):
-        return self.pub_date.date() == datetime.date.today()
+class Poll(ModelGZM):
+	question = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	def __unicode__(self):
+		return self.question
+	def was_published_today(self):
+		return self.pub_date.date() == datetime.date.today()
 
-    was_published_today.short_description = 'Published today?'
+	was_published_today.short_description = 'Published today?'
 
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
-    def __unicode__(self):
-        return self.choice
+class Choice(ModelGZM):
+	poll = models.ForeignKey(Poll)
+	choice = models.CharField(max_length=200)
+	votes = models.IntegerField()
+	def __unicode__(self):
+		return self.choice
 
 #S: Ponele los puntos
 #S: definiciones
-class VariableDef(models.Model):
-    """ej. cuanto tardo en llegar"""
-    desc = models.CharField(max_length=100)
-    def __unicode__(self):
-        return self.desc
+class VariableDef(ModelGZM):
+	"""ej. cuanto tardo en llegar"""
+	desc = models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.desc
 
-    def optionWithDesc(self, desc):
-        """ej. si self=puntualidad, devolveme la opcion 1h tarde"""
-        return self.variableopt_set.all().filter(desc=desc)[0]
+	def optionWithDesc(self, desc):
+		"""ej. si self=puntualidad, devolveme la opcion 1h tarde"""
+		return self.variableopt_set.all().filter(desc=desc)[0]
 
-class VariableOpt(models.Model):
-    """ej. tardo en llegar=20min"""
-    variableDef= models.ForeignKey(VariableDef)
-    desc= models.CharField(max_length=20)
-    def __unicode__(self):
-        return self.desc
+class VariableOpt(ModelGZM):
+	"""ej. tardo en llegar=20min"""
+	variableDef= models.ForeignKey(VariableDef)
+	desc= models.CharField(max_length=20)
+	def __unicode__(self):
+		return self.desc
 
-class Rubro(models.Model):
-    """ej. Plomero"""
-    desc = models.CharField(max_length=100)
-    def __unicode__(self):
-        return self.desc
+class Rubro(ModelGZM):
+	"""ej. Plomero"""
+	desc = models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.desc
 
-class VariableParaRubro(models.Model):
-    """ej. para Plomero 'cuanto tardo en llegar'"""
-    rubro= models.ForeignKey(Rubro)
-    variableDef= models.ForeignKey(VariableDef)
+class VariableParaRubro(ModelGZM):
+	"""ej. para Plomero 'cuanto tardo en llegar'"""
+	rubro= models.ForeignKey(Rubro)
+	variableDef= models.ForeignKey(VariableDef)
 
 #S: instancias
-class Candidato(models.Model):
-    """ej. Juan Plomero"""
-    desc = models.CharField(max_length=100)
-    rubro= models.ForeignKey(Rubro)
-    #XXX: telefono, mail, otras formas de IDENTIFICARLO
-    def __unicode__(self):
-        return self.desc
+class Candidato(ModelGZM):
+	"""ej. Juan Plomero"""
+	desc = models.CharField(max_length=100)
+	rubro= models.ForeignKey(Rubro)
+	#XXX: telefono, mail, otras formas de IDENTIFICARLO
+	def __unicode__(self):
+		return self.desc
 
-class Voto(models.Model):
-    """ej. Juan Plomero tardo 20 min en llegar"""
-    #XXX: solo vale la pena si queremos distinguir por votante, PERO habria que agregar quien es el votante y agrupar los votos en una "votacion"
-    candidato= models.ForeignKey(Candidato)
-    variableOpt= models.ForeignKey(VariableOpt)
-    def __unicode__(self):
-        return self.variableOpt.desc
+class TipoIdentificador(ModelGZM):
+	"""ej. 15 6242 2222 identifica a Mauricio"""
+	desc = models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.desc
 
-class VotoCount(models.Model):
-    """ej. 100 personas dijeron que Juan Plomero tardo 20 min en llegar"""
-    candidato= models.ForeignKey(Candidato)
-    variableOpt= models.ForeignKey(VariableOpt)
-    cnt= models.IntegerField()
-    def __unicode__(self):
-        return self.candidato.desc + "/" + self.variableOpt.desc + "=" + str(self.cnt)
+class Identificador(ModelGZM):
+	"""ej. 15 6242 2222 identifica a Mauricio"""
+	desc = models.CharField(max_length=100)
+	tipo= models.ForeignKey(TipoIdentificador)
+	def __unicode__(self):
+		return self.desc
+
+class IdentificadorCandidato(ModelGZM):
+	identificador = models.ForeignKey(Identificador)
+	candidato = models.ForeignKey(Candidato)
+	def __unicode__(self):
+		return identificador.desc + " -> " + candidato.desc
+
+class Voto(ModelGZM):
+	"""ej. Juan Plomero tardo 20 min en llegar"""
+	#XXX: solo vale la pena si queremos distinguir por votante, PERO habria que agregar quien es el votante y agrupar los votos en una "votacion"
+	candidato= models.ForeignKey(Candidato)
+	variableOpt= models.ForeignKey(VariableOpt)
+	def __unicode__(self):
+		return self.variableOpt.desc
+
+class VotoCount(ModelGZM):
+	"""ej. 100 personas dijeron que Juan Plomero tardo 20 min en llegar"""
+	candidato= models.ForeignKey(Candidato)
+	variableOpt= models.ForeignKey(VariableOpt)
+	cnt= models.IntegerField()
+	def __unicode__(self):
+		return self.candidato.desc + "/" + self.variableOpt.desc + "=" + str(self.cnt)
 
 
 #S: metodos comodos
 def VariableDefCreate(desc, opts):
-    varDef= VariableDef(desc=desc)
-    varDef.save()
+	varDef= VariableDef(desc=desc)
+	varDef.save()
 
-    for o in opts:
-      varDef.variableopt_set.create(desc=o)
+	for o in opts:
+	  varDef.variableopt_set.create(desc=o)
 
-    return varDef
+	return varDef
 
 def RubroCreate(desc, varDefNames):
 
-    try: 
-        rubro= Rubro.objects.get(desc=desc)
-    except ObjectDoesNotExist:
-        rubro= Rubro(desc=desc)
-        rubro.save()
+	try: 
+		rubro= Rubro.objects.get(desc=desc)
+	except ObjectDoesNotExist:
+		rubro= Rubro(desc=desc)
+		rubro.save()
 
-    for o in varDefNames:
-      vardef= VariableDef.objects.get(desc=o)
-      rubro.variablepararubro_set.create(variableDef= vardef)
+	for o in varDefNames:
+	  vardef= VariableDef.objects.get(desc=o)
+	  rubro.variablepararubro_set.create(variableDef= vardef)
 
-    return rubro
+	return rubro
 
 def votoCntForCandidato(candidato):
-    vv= dict()
-    for optCnt in candidato.votocount_set.all():
-        vv[optCnt.variableOpt.variableDef.desc +"/"+ optCnt.variableOpt.desc]= optCnt.cnt
-    return vv
+	vv= dict()
+	for optCnt in candidato.votocount_set.all():
+		vv[optCnt.variableOpt.variableDef.desc +"/"+ optCnt.variableOpt.desc]= optCnt.cnt
+	return vv
  
 
 def votarDefOptForCandidato(candidato, varDefName, varOptName):
-    varDef= candidato.rubro.variablepararubro_set.get(variableDef__desc= varDefName).variableDef
-    varOpt= varDef.variableopt_set.get(desc=varOptName)
-    return votarOptForCandidato(candidato, varOpt)
+	varDef= candidato.rubro.variablepararubro_set.get(variableDef__desc= varDefName).variableDef
+	varOpt= varDef.variableopt_set.get(desc=varOptName)
+	return votarOptForCandidato(candidato, varOpt)
 
 @transaction.commit_on_success
 def votarOptForCandidato(candidato, varOpt):
-    try:
-        vc= candidato.votocount_set.get(variableOpt= varOpt)
-        vc.cnt= vc.cnt+1
-        vc.save()
-    except ObjectDoesNotExist:
-        candidato.votocount_set.create(variableOpt= varOpt, cnt= 1)
+	try:
+		vc= candidato.votocount_set.get(variableOpt= varOpt)
+		vc.cnt= vc.cnt+1
+		vc.save()
+	except ObjectDoesNotExist:
+		candidato.votocount_set.create(variableOpt= varOpt, cnt= 1)
 
+@transaction.commit_on_success
+def identificadorForCandidatoRegistrar(candidato, iden, tipo):
+	"""registra un identificador para un candidato UNA SOLA VEZ"""
+	isNew= False
+
+	tipoO= TipoIdentificador.objects.get(desc= tipo)
+	idenO= None
+	try:
+		idenO= Identificador.objects.get(desc= iden, tipo= tipoO)
+		try:
+			IdentificadorCandidato.objects.get(candidato=candidato, identificador= idenO)
+		except ObjectDoesNotExist:
+			isNew= True
+	except ObjectDoesNotExist:
+		idenO= Identificador(desc= iden, tipo= tipoO)
+		idenO.save()
+		isNew= True
+
+	if isNew:
+		IdentificadorCandidato(candidato= candidato, identificador= idenO).save()
--- polls/tests.py
+++ polls/tests.py
@@ -11,11 +11,11 @@ from  polls.models import *
 from django.core.exceptions import ObjectDoesNotExist
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+	def test_basic_addition(self):
+		"""
+		Tests that 1 + 1 always equals 2.
+		"""
+		self.failUnlessEqual(1 + 1, 2)
 
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
@@ -27,61 +27,95 @@ True
 
 class VotarYContarTest(TestCase):
 
-    def test_votos_se_suman(self):
-        varDef= VariableDefCreate('XCategoria1',['Xopt1','XOpt2','Xopt3'])
-        varOpt2= varDef.optionWithDesc('XOpt2')
+	def test_votos_se_suman(self):
+		varDef= VariableDefCreate('XCategoria1',['Xopt1','XOpt2','Xopt3'])
+		varOpt2= varDef.optionWithDesc('XOpt2')
 
-        rubro1= RubroCreate("XRubro",["XCategoria1"])
+		rubro1= RubroCreate("XRubro",["XCategoria1"])
 
-        candidato1= Candidato(desc="XCandidato1", rubro= rubro1)
-        candidato1.save()
+		candidato1= Candidato(desc="XCandidato1", rubro= rubro1)
+		candidato1.save()
 
-        votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
-        vv= votoCntForCandidato(candidato1)
-        self.failUnlessEqual(vv["XOpt2"], 1)
+		votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
+		vv= votoCntForCandidato(candidato1)
+		self.failUnlessEqual(vv["XCategoria1/XOpt2"], 1)
 
-        votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
-        vv= votoCntForCandidato(candidato1)
-        self.failUnlessEqual(vv["XOpt2"], 2)
+		votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
+		vv= votoCntForCandidato(candidato1)
+		self.failUnlessEqual(vv["XCategoria1/XOpt2"], 2)
 
-    def test_no_vota_si_no_es_del_rubro(self):
-        varDef= VariableDefCreate('XCategoria2',['Xopt1','XOpt2','Xopt3'])
-        varOpt2= varDef.optionWithDesc('XOpt2')
+	def test_no_vota_si_no_es_del_rubro(self):
+		varDef= VariableDefCreate('XCategoria2',['Xopt1','XOpt2','Xopt3'])
+		varOpt2= varDef.optionWithDesc('XOpt2')
 
-        rubro1= RubroCreate("XRubro",["XCategoria2"])
+		rubro1= RubroCreate("XRubro",["XCategoria2"])
 
-        candidato1= Candidato(desc="XCandidato2", rubro= rubro1)
-        candidato1.save()
+		candidato1= Candidato(desc="XCandidato2", rubro= rubro1)
+		candidato1.save()
 
-       	try: 
-            votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
-            self.fail("Voto por una categoria que NO es del rubro")
-        except ObjectDoesNotExist:
-            pass
-        except:
-            self.fail("Voto por una categoria que No es del rubro y NO recibio DoesNotExist")
+		try: 
+			votarDefOptForCandidato(candidato1, "XCategoria1", "XOpt2")
+			self.fail("Voto por una categoria que NO es del rubro")
+		except ObjectDoesNotExist:
+			pass
+		except:
+			self.fail("Voto por una categoria que No es del rubro y NO recibio DoesNotExist")
 
-       	try: 
-            votarDefOptForCandidato(candidato1, "XCategoria2", "XOptNoExiste")
-            self.fail("Voto por una opcion que NO es de la categoria")
-        except ObjectDoesNotExist:
-            pass
-        except:
-            self.fail("Voto por una opcion que No es del rubro y NO recibio DoesNotExist")
+	   	try: 
+			votarDefOptForCandidato(candidato1, "XCategoria2", "XOptNoExiste")
+			self.fail("Voto por una opcion que NO es de la categoria")
+		except ObjectDoesNotExist:
+			pass
+		except:
+			self.fail("Voto por una opcion que No es del rubro y NO recibio DoesNotExist")
 
-    def test_rubro_crear(self):
-        varDef= VariableDefCreate('XCategoriaRubro',['Xopt1','XOpt2','Xopt3'])
-        RubroCreate('XRubroNuevo',['XCategoriaRubro'])
+	def test_rubro_crear(self):
+		varDef= VariableDefCreate('XCategoriaRubro',['Xopt1','XOpt2','Xopt3'])
+		RubroCreate('XRubroNuevo',['XCategoriaRubro'])
 
-	rr= Rubro.objects.filter(desc='XRubroNuevo')
-        self.failUnlessEqual(len(rr),1)
+		rr= Rubro.objects.filter(desc='XRubroNuevo')
+		self.failUnlessEqual(len(rr),1)
 
-        varDef= VariableDefCreate('XCategoriaRubro2',['Xopt1','XOpt2','Xopt3'])
-        RubroCreate('XRubroNuevo',['XCategoriaRubro2'])
+		varDef= VariableDefCreate('XCategoriaRubro2',['Xopt1','XOpt2','Xopt3'])
+		RubroCreate('XRubroNuevo',['XCategoriaRubro2'])
 
-	rr= Rubro.objects.filter(desc='XRubroNuevo')
-        self.failUnlessEqual(len(rr),1)
-	try:
-	    rr[0].variablepararubro_set.get(variableDef__desc='XCategoriaRubro2')
-        except ObjectDoesNotExist:
-            self.fail("No agrego la categoria al rubro")
+		rr= Rubro.objects.filter(desc='XRubroNuevo')
+		self.failUnlessEqual(len(rr),1)
+		try:
+			rr[0].variablepararubro_set.get(variableDef__desc='XCategoriaRubro2')
+		except ObjectDoesNotExist:
+			self.fail("No agrego la categoria al rubro")
+
+	def test_identificador(self):
+		ttel= TipoIdentificador(desc="telefono").save()
+		tmail= TipoIdentificador(desc="email").save()
+
+		varDef= VariableDefCreate('XCategoriaRubro2',['Xopt1','XOpt2','Xopt3'])
+		rubro= RubroCreate('XRubroNuevo',['XCategoriaRubro2'])
+ 
+		c1= Candidato(desc="XCandidato1", rubro=rubro ).save()
+		c2= Candidato(desc="XCandidato2", rubro=rubro ).save()
+		
+		identificadorForCandidatoRegistrar(c1,"4444-4441","telefono")
+		identificadorForCandidatoRegistrar(c2,"4444-4442","telefono")
+		identificadorForCandidatoRegistrar(c2,"4444-4442","telefono")
+		identificadorForCandidatoRegistrar(c2,"candidato2@dominio.com","email")
+		identificadorForCandidatoRegistrar(c2,"spammer@dominio.com","email")
+		identificadorForCandidatoRegistrar(c1,"spammer@dominio.com","email")
+
+		r1= IdentificadorCandidato.objects.filter(identificador__tipo__desc="telefono", identificador__desc="4444-4441")
+		self.failUnlessEqual(len(r1),1)
+		self.failUnlessEqual(r1[0].candidato, c1)
+
+		r1= IdentificadorCandidato.objects.filter(identificador__tipo__desc="telefono", identificador__desc="4444-4442")
+		self.failUnlessEqual(len(r1),1)
+		self.failUnlessEqual(r1[0].candidato, c2)
+
+		r1= IdentificadorCandidato.objects.filter(identificador__tipo__desc="email", identificador__desc="spammer@dominio.com")
+		self.failUnlessEqual(len(r1),2)
+		if (r1[0].candidato== c1 and r1[1].candidato== c2) or (r1[1].candidato== c1 and r1[0].candidato== c2):
+			pass
+		else:
+			self.fail("spammer@dominio.com no trajo los candidatos correctos")
+
+
