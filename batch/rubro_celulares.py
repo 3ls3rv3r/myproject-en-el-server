b20797f16c2c5e46b83b9cde93c7bacafe367c3c
na <na@na-desktop.(none)>
2010-10-22 10:19:48 -0300

    incorpora ISPs y celulares en los batch

--- batch/rubro_celulares.py
+++ batch/rubro_celulares.py
@@ -1,13 +1,12 @@
-"""rubro proveedores de internet en capital segun Gonzalo"""
+"""rubro proveedores de telefonia celular segun Gonzalo"""
 from  polls.models import *
 
 def categorias(self):
-        varDef= VariableDefCreate('demora para instalar',['dentro de las 48hs','dentro de los 10 dias','dentro del mes','Nunca vinieron']) 
+        varDef= VariableDefCreate('habilitacion de linea',['inmediata','dentro de las 24 horas',,'dentro de las 48 horas', ,'mas de dos dias']) 
         varDef= VariableDefCreate('cortes de servicio',['nunca','uno por mes','uno por semana','uno por dia'])
         varDef= VariableDefCreate('soporte tecnico ante incidentes',['solucion inmediata','solucion en 24hs','solucion en 48 hs','no dan solucion'])
         varDef= VariableDefCreate('atencion al cliente',['personalizada y eficiente','personalizada e ineficiente','automatizada y eficiente','automatizada e ineficiente'])
-        varDef= VariableDefCreate('baja del servicio',['tramite telefonico/mail','hay que ir a una oficina una vez','hay que ir varias veces'])
-	rubro1= RubroCreate("proveedores internet en capital",["demora para instalar","cortes de servicio","soporte tecnico ante incidentes","atencion al cliente","baja del servicio"])
+        rubro1= RubroCreate("proveedores internet en capital",["demora para instalar","cortes de servicio","soporte tecnico ante incidentes","atencion al cliente"])
 
         candidato1= Candidato(desc="Speedy", rubro= rubro1).save()
         candidato1= Candidato(desc="Arnet", rubro= rubro1).save()
